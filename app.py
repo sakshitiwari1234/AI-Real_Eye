38e23314e9a428e796d46490872437942a0892a4
OshankAgrawal <oshankagrawal@gmail.com>
2025-04-13 17:20:24 +0530

    Backend file updated

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ st.subheader("Detect whether an image is AI-Generated or Real")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    img = Image.open(uploaded_file)
+    img = Image.open(uploaded_file).convert("RGB")
     st.image(img, caption="Uploaded Image", use_column_width=True)
 
     # Preprocess image
-    img_resized = img.resize((256, 256))
+    img_resized = img.resize((224, 224))
     img_array = image.img_to_array(img_resized) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
